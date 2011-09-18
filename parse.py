d652770226428a1f0ae5c1430a1d82d8ae356b36
Aaron Mansheim <minopret@gmail.com>
2011-09-18 12:15:46 -0400

    Some minor improvements to deserialization and to output for Python and for Gallina (Coq).

--- parse.py
+++ parse.py
@@ -21,14 +21,45 @@ def pprint(sexpr):
 		else:
 			print '()',
 	else:
-		print sexpr,
+		print patom(sexpr),
 
-def atom_id(s):
+def patom(s):
+	s = s.replace('.', '')
+	if s in syms:
+		return s
+	else:
+		return "'" + s + "'"
+
+def atom_num(s):
 	d = 0
 	for c in s:
 		d *= 256
 		d += ord(c)
 	return d
+
+syms = set("evcon evlis atom eq cond car cdr cons assoc quote append null list pair and not cadr caddr caar cadar caddar eval lambda label".split())
+
+def atom_id(s):
+	s = s.replace('.', '')
+	if s in syms:
+		return "sym_" + s
+	else:
+		#return '[' + s + ']'
+		d = atom_num(s)
+		h = list(hex(d))[2:]
+		if h[-1] == 'L':
+			h = h[0:-1]
+		h.reverse()
+		hn = h[-1]
+		h = h[0:-1]
+		nh = len(h)
+		h = ' (d'.join(h)
+		h += ' h' + hn + (')' * nh)
+		#print h
+		s = "symbol (d"
+		s += h
+		return '(' + s + ')'
+		#return s
 
 def id_str(d):
 	s = ''
@@ -43,36 +74,58 @@ def gprint(sexpr):
 		if len(sexpr) == 0:
 			print 'nil ',
 		else:
-			print '(',
-			gprint(sexpr[0]),
-			for e in sexpr[1:]:
+			ns = len(sexpr)
+			while len(sexpr) > 1:
+				print '(cons ',
+				gprint(sexpr[0]),
 				print " ",
-				gprint(e)
-			print ')',
+				sexpr = sexpr[1:]
+			gprint(sexpr[0])
+			print ")" * (ns - 1),
 	else:
 		print atom_id(sexpr),
 
-f = open("minopret.lisp", "r")
+f = open("lisp3.lisp", "r")
 mnplisp = f.read()
 f.close()
 
 mnplisp = sub(r'^\s*;.*', r'', mnplisp, 0, flags=M)  # drop comment lines
 mnplisp = sub(r'\(', r'( ', mnplisp)  # insert space after open
-mnplisp = sub(r'\)', r' )', mnplisp)  # insert space before cloes
+mnplisp = sub(r'\)', r' )', mnplisp)  # insert space before close
 tokens = mnplisp.split()  # tokenize on whitespace (acknowledgment: Peter Norvig in 'lis.py')
 
-stack = [[]]
-level = 0
+expr = []
+stack = [expr,]
+
+def add_token(tok):
+	stack[-1].append(tok)
+
+def begin_list(quoted = False):
+	new_list = []
+	if quoted == True:
+		new_list.append("quote")
+		newer_list = []
+		new_list.append(newer_list)
+		stack[-1].append(new_list)
+		stack.append(new_list)
+		new_list = newer_list
+	stack[-1].append(new_list)
+	stack.append(new_list)
+	
+def end_list():
+	if len(stack) > 1 and len(stack[-2]) > 0 and stack[-2][0] == "quote":
+		stack.pop()
+	stack.pop()
+	
 for t in tokens:
 	if t == '(':
-		stack.append([])
-		level += 1
+		begin_list()
 	elif t == ')':
-		stack[level - 1].append(stack[level])
-		stack.pop()
-		level -= 1
+		end_list()
+	elif t == "'(":
+		begin_list(quoted = True)
 	else:
-		stack[level].append(t)
+		add_token(t)
 
 def for_python():
 	for sexpr in stack[0]:
