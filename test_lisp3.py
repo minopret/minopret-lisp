e5318f604acfba5aa24d3756aee570c1fbbcedab
Aaron Mansheim <minopret@gmail.com>
2011-09-18 12:34:35 -0400

    reformatted

--- test_lisp3.py
+++ test_lisp3.py
@@ -1,13 +1,12 @@
 from lisp3 import *
 
 # (cdr (quote (atom lambda)))
-print List(Symbol("cdr"), List(
-    List(Symbol("quote"), List(
-        List(Symbol("atom"), List(Symbol("lambda"), List.nil)),
-        List.nil)
-    ),
-    List.nil
-)).eval_([])
+print List( Symbol("cdr"), List(
+    List( Symbol("quote"), List(
+        List( Symbol("atom"), List( Symbol("lambda"), List.nil,
+        ),), List.nil,
+    ),), List.nil,
+),).eval_([])
 
 # Same thing constructed slightly differently.
 # car: "cdr"
@@ -15,11 +14,13 @@ print List(Symbol("cdr"), List(
 #      cdr: (nil) cdr: list> car: list> car: "atom"
 #                            cdr: (nil) cdr: car: "lambda"
 #                                            cdr: (nil)
-print   Symbol("cdr").cons_( List(
-          Symbol("quote").cons_( List(
-            Symbol("atom").cons_(Symbol("lambda").cons_(List.nil)),
-            List.nil
-          )),
-          List.nil
-        )).eval_([])
+print Symbol("cdr").cons_( List(
+    Symbol("quote").cons_( List(
+        Symbol("atom").cons_( Symbol("lambda").cons_(
+            List.nil,
+        ),),
+        List.nil,
+    ),),
+    List.nil,
+),).eval_([])
 
