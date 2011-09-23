21e682e93ef41330d9077c67955367064db9d2ff
Aaron Mansheim <minopret@gmail.com>
2011-09-23 17:33:14 -0400

    Big deal: builtins and repr make this a working interpreter

--- lisp3.py
+++ lisp3.py
@@ -57,6 +57,9 @@ class Expr():
 
 
 class Symbol(Expr):
+
+    prims = None
+    builtins = None
     
     def __init__(self, data):
         self.data = str(data)
@@ -79,11 +82,8 @@ class Symbol(Expr):
             return Symbol.s_t
         else:
             return List.nil
-            
-    def eval_(self, a):
-        return self.assoc_(a)
     
-    # Other built-ins
+    # Non-primitive built-ins
     def and_(self, e):
         if self == Symbol.s_t and e == Symbol.s_t:
             return Symbol.s_t
@@ -91,27 +91,47 @@ class Symbol(Expr):
             return List.nil
             
     def assoc_(self, a):
-        if a.caar_().eq_(self) == Symbol.s_t:
+        if a.null_() == Symbol.s_t:  # discovered fix
+            return List.nil
+        elif a.caar_().eq_(self) == Symbol.s_t:
             return a.cadar_()
         else:
             self.assoc_(a.cdr_())
 
+    def eval_(self, a):
+        return self.assoc_(a + Symbol.builtins)
+
     def __str__(self):
         return self.data
 
+    def __repr__(self):
+        return 'Symbol("' + self.data + '")'
+
 
 # Distinguished instances of Symbol
-Symbol.s_t = Symbol("t")
-Symbol.s_quote = Symbol("quote")
-Symbol.s_atom = Symbol("atom")
-Symbol.s_eq = Symbol("eq")
-Symbol.s_car = Symbol("car")
-Symbol.s_cdr = Symbol("cdr")
-Symbol.s_cons = Symbol("cons")
-Symbol.s_cond = Symbol("cond")
-Symbol.s_label = Symbol("label")
-Symbol.s_lambda = Symbol("lambda")
+Symbol.s_atom = Symbol('atom')
+Symbol.s_car = Symbol('car')
+Symbol.s_cdr = Symbol('cdr')
+Symbol.s_cond = Symbol('cond')
+Symbol.s_cons = Symbol('cons')
+Symbol.s_eq = Symbol('eq')
+Symbol.s_label = Symbol('label')
+Symbol.s_lambda = Symbol('lambda')
+Symbol.s_quote = Symbol('quote')
+Symbol.s_t = Symbol('t')
 
+Symbol.prims = {
+    'atom': Symbol.s_atom,
+    'car': Symbol.s_car,
+    'cdr': Symbol.s_cdr,
+    'cond': Symbol.s_cond,
+    'cons': Symbol.s_cons,
+    'eq': Symbol.s_eq,
+    'label': Symbol.s_label,
+    'lambda': Symbol.s_lambda,
+    'quote': Symbol.s_quote,
+    't': Symbol.s_t,
+}
 
 class List(Expr):
     nil = None
@@ -164,7 +184,10 @@ class List(Expr):
             return e
         else:
             return self.car_().cons_(self.cdr_().append_(e))
-            
+        
+    def assoc_(self, a):
+        return List.nil  # discovered fix
+
     def caar_(self):
         return self.car_().car_()
         
@@ -245,7 +268,174 @@ class List(Expr):
                 c = c.cdr_()
         s += ')'
         return s
+    
+    def __repr__(self):
+        s = ''
+        if self.null_() == Symbol.s_t:
+            s = 'List.nil'
+        else:
+            s += repr(self.car_())
+            s += '.cons_(' + repr(self.cdr_()) + ')'
+        return s
 
 # Create distinguished instance of List
 List()._mk_nil()
+
+# Create Lisp association list of Lisp definitions of builtins.
+# The following definitions were translated automatically
+# from Lisp s-expression code.
+Symbol.builtins = (
+
+# evlis
+Symbol("evlis").cons_(
+    Symbol.s_lambda.cons_(Symbol("m").cons_(Symbol("a").cons_(List.nil)).cons_(
+    Symbol.s_cond.cons_(
+    Symbol("null").cons_(Symbol("m").cons_(List.nil)).cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil)).cons_(
+    Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol.s_cons.cons_(Symbol("eval").cons_(Symbol.s_car.cons_(Symbol("m").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(Symbol("evlis").cons_(Symbol.s_cdr.cons_(Symbol("m").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+    )
+    ).cons_(List.nil)
+    )).cons_(List.nil)
+).cons_(
+
+# evcon
+Symbol("evcon").cons_(
+    Symbol.s_lambda.cons_(Symbol("c").cons_(Symbol("a").cons_(List.nil)).cons_(
+    Symbol.s_cond.cons_(
+    Symbol("eval").cons_(Symbol("caar").cons_(Symbol("c").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(Symbol("eval").cons_(Symbol("cadar").cons_(Symbol("c").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(
+    Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol("evcon").cons_(Symbol.s_cdr.cons_(Symbol("c").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+    )
+    ).cons_(List.nil))
+    ).cons_(List.nil)
+).cons_(
+
+# eval
+Symbol("eval").cons_(
+    Symbol.s_lambda.cons_(Symbol("e").cons_(Symbol("a").cons_(List.nil)).cons_(
+    Symbol.s_cond.cons_(
+    # bare atom
+    Symbol.s_atom.cons_(Symbol("e").cons_(List.nil)).cons_(
+        Symbol("assoc").cons_(Symbol("e").cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)
+    ).cons_(
+    # apply an atom
+    Symbol.s_atom.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(List.nil)).cons_(
+    Symbol.s_cond.cons_(
+        # quote
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_quote.cons_(List.nil)).cons_(List.nil))).cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(List.nil)).cons_(
+        # atom
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_atom.cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_atom.cons_(Symbol("eval").cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(List.nil)).cons_(
+        # eq
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_eq.cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_eq.cons_(Symbol("eval").cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(Symbol("eval").cons_(Symbol("caddr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(
+        # car
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_car.cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_car.cons_(Symbol("eval").cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(List.nil)).cons_(
+        # cdr
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_cdr.cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_cdr.cons_(Symbol("eval").cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(List.nil)).cons_(
+        # cons
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_cons.cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_cons.cons_(Symbol("eval").cons_(Symbol("cadr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(Symbol("eval").cons_(Symbol("caddr").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(
+        # cond
+        Symbol.s_eq.cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_cond.cons_(List.nil)).cons_(List.nil))).cons_(Symbol("evcon").cons_(Symbol.s_cdr.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(
+        # other atoms
+        Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol("eval").cons_(Symbol.s_cons.cons_(Symbol("assoc").cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(Symbol.s_cdr.cons_(Symbol("e").cons_(List.nil)).cons_(List.nil))).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+        ) # /cond
+        ) # /cons
+        ) # /cdr
+        ) # /car
+        ) # /eq
+        ) # /atom
+        ) # /quote
+    ).cons_(List.nil)).cons_(
+    # lambda
+    Symbol.s_eq.cons_(Symbol("caar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_lambda.cons_(List.nil)).cons_(List.nil))).cons_(Symbol("eval").cons_(Symbol("caddar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("append").cons_(Symbol("pair").cons_(Symbol("cadar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("evlis").cons_(Symbol.s_cdr.cons_(Symbol("e").cons_(List.nil)).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(
+    # label
+    Symbol.s_eq.cons_(Symbol("caar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_label.cons_(List.nil)).cons_(List.nil))).cons_(Symbol("eval").cons_(Symbol.s_cons.cons_(Symbol("caddar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_cdr.cons_(Symbol("e").cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_cons.cons_(Symbol("list").cons_(Symbol("cadar").cons_(Symbol("e").cons_(List.nil)).cons_(Symbol.s_car.cons_(Symbol("e").cons_(List.nil)).cons_(List.nil))).cons_(Symbol("a").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+    ) # /lambda
+    ) # /apply an atom
+    ) # /bare atom
+).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# assoc
+Symbol("assoc").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(Symbol("y").cons_(List.nil)).cons_(
+Symbol.s_cond.cons_(Symbol.s_eq.cons_(Symbol("caar").cons_(Symbol("y").cons_(List.nil)).cons_(Symbol("x").cons_(List.nil))).cons_(Symbol("cadar").cons_(Symbol("y").cons_(List.nil)).cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol("assoc").cons_(Symbol("x").cons_(Symbol.s_cdr.cons_(Symbol("y").cons_(List.nil)).cons_(List.nil))).cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# pair
+Symbol("pair").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(Symbol("y").cons_(List.nil)).cons_(
+Symbol.s_cond.cons_(
+Symbol("and").cons_(Symbol("null").cons_(Symbol("x").cons_(List.nil)).cons_(Symbol("null").cons_(Symbol("y").cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil)).cons_(
+Symbol("and").cons_(Symbol("not").cons_(Symbol.s_atom.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(Symbol("not").cons_(Symbol.s_atom.cons_(Symbol("y").cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(Symbol.s_cons.cons_(Symbol("list").cons_(Symbol.s_car.cons_(Symbol("x").cons_(List.nil)).cons_(Symbol.s_car.cons_(Symbol("y").cons_(List.nil)).cons_(List.nil))).cons_(Symbol("pair").cons_(Symbol.s_cdr.cons_(Symbol("x").cons_(List.nil)).cons_(Symbol.s_cdr.cons_(Symbol("y").cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+)
+).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# append
+Symbol("append").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(Symbol("y").cons_(List.nil)).cons_(
+Symbol.s_cond.cons_(Symbol("null").cons_(Symbol("x").cons_(List.nil)).cons_(Symbol("y").cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol.s_cons.cons_(Symbol.s_car.cons_(Symbol("x").cons_(List.nil)).cons_(Symbol("append").cons_(Symbol.s_cdr.cons_(Symbol("x").cons_(List.nil)).cons_(Symbol("y").cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# caddar
+Symbol("caddar").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_car.cons_(Symbol.s_cdr.cons_(Symbol.s_cdr.cons_(Symbol.s_car.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# cadar
+Symbol("cadar").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_car.cons_(Symbol.s_cdr.cons_(Symbol.s_car.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# caar
+Symbol("caar").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_car.cons_(Symbol.s_car.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# caddr
+Symbol("caddr").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_car.cons_(Symbol.s_cdr.cons_(Symbol.s_cdr.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# cadr
+Symbol("cadr").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_car.cons_(Symbol.s_cdr.cons_(Symbol("x").cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# list
+Symbol("list").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(Symbol("y").cons_(List.nil)).cons_(Symbol.s_cons.cons_(Symbol("x").cons_(Symbol.s_cons.cons_(Symbol("y").cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# not
+Symbol("not").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(
+Symbol.s_cond.cons_(Symbol("x").cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# and
+Symbol("and").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(Symbol("y").cons_(List.nil)).cons_(
+Symbol.s_cond.cons_(Symbol("x").cons_(
+Symbol.s_cond.cons_(Symbol("y").cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil)).cons_(Symbol.s_quote.cons_(Symbol.s_t.cons_(List.nil)).cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)
+).cons_(
+
+# null
+Symbol("null").cons_(
+Symbol.s_lambda.cons_(Symbol("x").cons_(List.nil).cons_(Symbol.s_eq.cons_(Symbol("x").cons_(Symbol.s_quote.cons_(List.nil.cons_(List.nil)).cons_(List.nil))).cons_(List.nil))).cons_(List.nil)).cons_(List.nil)
+
+) # /and
+) # /not
+) # /list
+) # /cadr
+) # /caddr
+) # /caar
+) # /caddar
+) # /cadar
+) # /append
+) # /pair
+) # /assoc
+) # /eval
+) # /evcon
+) # /evlis
+) # /Symbol.builtins
+
 
