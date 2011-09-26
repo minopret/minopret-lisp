8a02d67209ba002ed81aa8d12380fd3c6d097c00
Aaron Mansheim <minopret@gmail.com>
2011-09-26 00:32:26 -0400

    deal in tuples not lists; do not print non-result of label

--- repyl1.py
+++ repyl1.py
@@ -3,15 +3,17 @@ from sys import stdin
 def repl(prompt='repyl1> '):
     from lispy1 import eval_
     # from parse3 import read_exprs  # for interactive prompt
-    from parse3 import string_to_list  # for input from file
+    from parse3 import string_to_tuple  # for input from file
     import traceback
     
     while True:
         try:
             # x = read_exprs(prompt = prompt)  # for interactive prompt
-            x = string_to_list(stdin.read())  # for input from file
+            x = string_to_tuple(stdin.read())  # for input from file
             for xi in x:
-                print eval_(xi)
+                yi = eval_(xi)
+                if yi != None:
+                    print yi
         except EOFError:
             break
         except Exception:
