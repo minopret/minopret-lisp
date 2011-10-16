b0706b8882cbd5a735a005fe9e4b72daa03fc0b2
Aaron Mansheim <minopret@gmail.com>
2011-10-15 23:57:16 -0400

    Drop ugly commented-out examples

--- mnpexpr.py
+++ mnpexpr.py
@@ -21,19 +21,3 @@ class Expr(tuple):
             s += ')'
         return s
 
-#x = Expr(('cons', Expr(('cons', Expr(('quote', Expr(('a', 'c')), )), Expr(('cons', 'b', Expr(), )), )), Expr(), ))
-#print x
-#y = Expr(('quote', Expr(('a', 'c')), ))
-#print y
-#print Expr() == Expr()
-#print Expr() is Expr()
-#print Expr().__hash__(), Expr().__hash__()
-#print id(Expr()), id(Expr())
-#print Expr() == ()
-#print Expr() is ()
-#print Symbol('') == Symbol('')  # True
-#print Symbol('') is Symbol('')  # True
-#print Symbol('').__hash__(), Symbol('').__hash__()  # 0 0
-#print id(Symbol('')), id(Symbol('')), id(''), id('')  # 0 0
-#print Symbol('') == ('')  # True
-#print Symbol('') is ('')  # True
