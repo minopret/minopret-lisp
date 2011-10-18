c31a9b597f3c67e3dec18c7caba7b2d3ee2d0c72
Aaron Mansheim <minopret@gmail.com>
2011-10-18 17:57:01 -0400

    add --script  option in scripts

--- test/test_binary.sh
+++ test/test_binary.sh
@@ -3,4 +3,4 @@ MNPBASE=`dirname $0`/..
 MNPLIB=$MNPBASE/lib
 cd $MNPLIB
 MNPTEST=../test
-cat lisp.lisp lib.lisp integer.lisp binary.lisp $MNPTEST/test_binary.lisp | python mnplisp.py
+cat lisp.lisp lib.lisp integer.lisp binary.lisp $MNPTEST/test_binary.lisp | python mnplisp.py --script
