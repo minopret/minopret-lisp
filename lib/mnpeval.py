393c7e58a7860b6cad128051b7bd66ead1c9b4be
Aaron Mansheim <minopret@gmail.com>
2011-10-23 15:56:21 -0400

    added proper tail calls: not so easy to hit the recursion limit any more

--- lib/mnpeval.py
+++ lib/mnpeval.py
@@ -120,91 +120,106 @@ def boolean(x):
     return True if x != Expr() else False
 
 
+class Procedure(object):
+    def __init__(self, params, body, env):
+        self.params = params
+        self.body = body
+        self.env = env
+    def __call__(self, *args):
+        return eval_(self.body, Env(self.params, args, self.env))
+
+
 env0 = mk_builtins(Env())
 
 
 def eval_(x, env=env0):
     from sys import exit
 
-    if trace:
-        print 'Evaluate ' + str(x) + '.'
-
-    if isinstance(x, Symbol) or x == Expr():  # (atom x)
-        e = env.find(x)
-        return (e[x] if e != None else x)
-
-    elif x[0] == 'quote':
-        exp = x[1]
-        return exp
-
-    elif x[0] == 'trace':
-        exp = x[1]
-        val = eval_(exp, env)  # near-miss tail call
-        print 'Trace ' + str(exp) + ': ' + str(val) + '.'
-        return val
-
-    elif x[0] == 'cond':
-        pairs = x[1:]
-        if len(pairs) == 0:        # In other implementations is it an error?
-            return Expr()
-        else:
-            (test, action) = pairs[0]
-            if not boolean(eval_(test, env)):  # NOT NEARLY a tail call
-                action = Expr([x[0]] + list(x[2:]))
-            return eval_(action, env)  # tail call
-
-    elif x[0] == 'label':
-        name = x[1]
-        value = x[2]
-        env[name] = eval_(value, env)  # tail call
-
-    # Actually Paul Graham writes 'label' in Common Lisp as:
-    #((eq (caar e) 'label)
-    #     (eval. (cons (caddar e) (cdr e))
-    #            (cons (list. (cadar e) (car e)) a)))
-    #
-    # Example of use (??)
-    # ( (label f ( (lambda (x) (cond ((null x) ()) (t (cdr x)))) ))  '(a b) )
-    # # which in Python might be: # elif x[0][0] == 'label':
-    #     label_expr = x[0]
-    #     args = list(x[1:])  # convert from Expr
-    #     label_name = x[0][1]
-    #     label_value = x[0][2]
-    #     env1 = Env((label_name), (label_expr), env)
-    #     return eval_(Expr([(label_value)] + args), env1)
-    #
-    # but could instead destructively update the existing environment:
-    # elif x[0][0] == 'label':
-    #     label_name = x[0][1]
-    #     label_value = x[0][2]
-    #     label_expr = x[0]
-    #     args = list(x[1:])  # convert from Expr
-    #     env[label_name] = label_expr
-    #     return eval_(Expr([(label_value)] + args), env)
-
-    elif x[0] == 'lambda':
-        params = x[1]
-        body = x[2]
-        return lambda_(params, body, env)
-
-    # Actually Paul Graham writes 'lambda' in Common Lisp as:
-    #((eq (caar e) 'lambda)
-    #     (eval. (caddar e)
-    #            (append. (pair. (cadar e) (evlis. (cdr e) a))
-    #                             a)))
-    # which in Python might be:
-    # elif x[0][0] == 'lambda':
-    #     y = [eval_(xi, env) for xi in x[1:]]
-    #     env1 = Env(x[0][1], y, env)
-    #     return eval_(x[0][2], env1)
-
-    elif x[0] == 'quit':
-        exit()
-
-    else:
-        y = [eval_(xi, env) for xi in x]  # NOT tail calls
-        y0 = y.pop(0)  # EXCEPT y0 is a near-miss tail call
-        y = Expr(y)
+    while True:  # Be ready to reprocess any proper tail calls.
         if trace:
-            print 'Apply ' + str(y0) + ' to ' + str(y) + '.'
-        return y0(*y)
+            print 'Evaluate ' + str(x) + '.'
+
+        if isinstance(x, Symbol) or x == Expr():  # (atom x)
+            e = env.find(x)
+            return (e[x] if e != None else x)
+
+        elif x[0] == 'quote':
+            exp = x[1]
+            return exp  # No tail call needed.
+
+        elif x[0] == 'trace':
+            exp = x[1]
+            val = eval_(exp, env)  # near-miss tail call
+            print 'Trace ' + str(exp) + ': ' + str(val) + '.'
+            return val
+
+        elif x[0] == 'cond':
+            pairs = x[1:]
+            if len(pairs) == 0:     # In other implementations is it an error?
+                return Expr()
+            else:
+                (test, action) = pairs[0]
+                if not boolean(eval_(test, env)):  # NOT NEARLY a tail call
+                    action = Expr([x[0]] + list(x[2:]))
+                x = action  # proper tail call
+
+        elif x[0] == 'label':
+            name = x[1]
+            value = x[2]
+            env[name] = eval_(value, env)  # near-miss tail call
+            return None
+
+        # Actually Paul Graham writes 'label' in Common Lisp as:
+        #((eq (caar e) 'label)
+        #     (eval. (cons (caddar e) (cdr e))
+        #            (cons (list. (cadar e) (car e)) a)))
+        #
+        # Example of use (??)
+        # ( (label f ( (lambda (x) (cond ((null x) ()) (t (cdr x)))) ))  '(a b) )
+        # # which in Python might be: # elif x[0][0] == 'label':
+        #     label_expr = x[0]
+        #     args = list(x[1:])  # convert from Expr
+        #     label_name = x[0][1]
+        #     label_value = x[0][2]
+        #     env1 = Env((label_name), (label_expr), env)
+        #     return eval_(Expr([(label_value)] + args), env1)
+        #
+        # but could instead destructively update the existing environment:
+        # elif x[0][0] == 'label':
+        #     label_name = x[0][1]
+        #     label_value = x[0][2]
+        #     label_expr = x[0]
+        #     args = list(x[1:])  # convert from Expr
+        #     env[label_name] = label_expr
+        #     return eval_(Expr([(label_value)] + args), env)
+
+        elif x[0] == 'lambda':
+            params = x[1]
+            body = x[2]
+            return Procedure(params, body, env)
+
+        # Actually Paul Graham writes 'lambda' in Common Lisp as:
+        #((eq (caar e) 'lambda)
+        #     (eval. (caddar e)
+        #            (append. (pair. (cadar e) (evlis. (cdr e) a))
+        #                             a)))
+        # which in Python might be:
+        # elif x[0][0] == 'lambda':
+        #     y = [eval_(xi, env) for xi in x[1:]]
+        #     env1 = Env(x[0][1], y, env)
+        #     return eval_(x[0][2], env1)
+
+        elif x[0] == 'quit':
+            exit()
+
+        else:
+            y = [eval_(xi, env) for xi in x]  # NOT tail calls
+            y0 = y.pop(0)
+            y = Expr(y)
+            if trace:
+                print 'Apply ' + str(y0) + ' to ' + str(y) + '.'
+            if isinstance(y0, Procedure):
+                x = y0.body
+                env = Env(y0.params, y, y0.env)  # proper tail call
+            else:
+                return y0(*y)  # apply a Python function from env
