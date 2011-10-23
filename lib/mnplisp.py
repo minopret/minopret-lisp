fb302c927210154e9f1d49f606838fc3392e6215
Aaron Mansheim <minopret@gmail.com>
2011-10-23 15:54:37 -0400

    keep up with mnpread; moved top level to main()

--- lib/mnplisp.py
+++ lib/mnplisp.py
@@ -2,57 +2,35 @@
 #
 # Aaron Mansheim, September 2011
 
-from sys import stdin, exit
-from argparse import ArgumentParser
-from mnpeval import set_trace
 
-arg_parser = ArgumentParser(description='A simple classic Lisp.')
-arg_parser.add_argument(
-    '--script',
-    dest='input',
-    action='store_const',
-    const='batch',
-    default='interactive',
-    help='process a batch of input with no prompt ' \
-        + '(default: prompt for interactive input)',
-)
+def read_exprs(prompt=None):
+    from sys import stdin
+    from mnpread import read_next_exprs, string_to_expr
 
-arg_parser.add_argument(
-    '--trace',
-    dest='trace',
-    action='store_const',
-    const='tron',
-    default='troff',
-    help='list every function as it is evaluated or applied',
-)
-
-args = arg_parser.parse_args()
-if args.trace == 'tron':
-    set_trace(True)
+    if prompt == None:
+        x = string_to_expr(stdin.read())  # input from file
+    else:
+        x = read_next_exprs(prompt=prompt)  # interactive prompt
+    return x
 
 
 def read_eval_print(prompt=None):
+    from sys import exit
     from mnpeval import eval_
-    from mnpread import read_exprs  # for interactive prompt
-    from mnpread import string_to_expr  # for input from file
     import traceback
 
     try:
-
-        if prompt == None:
-            x = string_to_expr(stdin.read())  # input from file
-        else:
-            x = read_exprs(prompt=prompt)  # interactive prompt
+        x = read_exprs(prompt=prompt)
 
         for xi in x:
 
             # Macros go here. I'm not enthusiastic about macros.
             # However, they seem the most convenient way by far to
             # implement (load).
-            if isinstance(xi, list) and len(xi) > 1 and xi[0] == 'load':
-                basename = xi[1]
-                # Perhaps check whether the file has been loaded previously.
-                # Handle input from the referenced file here.
+            #if isinstance(xi, list) and len(xi) > 1 and xi[0] == 'load':
+            #    basename = xi[1]
+            #    # Perhaps check whether the file has been loaded previously.
+            #    # Handle input from the referenced file here.
 
             yi = eval_(xi)
             if yi != None:
@@ -69,7 +47,40 @@ def read_eval_print_loop(prompt='mnplisp> '):
     while True:
         read_eval_print(prompt)
 
-if args.input == 'interactive':
-    read_eval_print_loop()
-else:
-    read_eval_print()
+
+def main():
+    from argparse import ArgumentParser
+    from mnpeval import set_trace
+
+    arg_parser = ArgumentParser(description='A simple classic Lisp.')
+    arg_parser.add_argument(
+        '--script',
+        dest='input',
+        action='store_const',
+        const='batch',
+        default='interactive',
+        help='process a batch of input with no prompt ' \
+            + '(default: prompt for interactive input)',
+    )
+
+    arg_parser.add_argument(
+        '--trace',
+        dest='trace',
+        action='store_const',
+        const='tron',
+        default='troff',
+        help='list every function as it is evaluated or applied',
+    )
+
+    args = arg_parser.parse_args()
+    if args.trace == 'tron':
+        set_trace(True)
+
+    if args.input == 'interactive':
+        read_eval_print_loop()
+    else:
+        read_eval_print()
+
+
+if __name__ == "__main__":
+    main()
