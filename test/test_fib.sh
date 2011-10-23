a94627a49ef20c3d014219f55d4e2b1b3390e1dd
Aaron Mansheim <minopret@gmail.com>
2011-10-23 16:03:27 -0400

    pass options through

--- test/test_fib.sh
+++ test/test_fib.sh
@@ -3,4 +3,4 @@ MNPBASE=`dirname $0`/..
 MNPLIB=$MNPBASE/lib
 cd $MNPLIB
 MNPTEST=../test
-cat lisp.lisp lib.lisp integer.lisp fib.lisp $MNPTEST/test_fib.lisp | python mnplisp.py --script
+cat lisp.lisp lib.lisp integer.lisp fib.lisp $MNPTEST/test_fib.lisp | python mnplisp.py --script $*
--- test/test_mult.sh
+++ test/test_mult.sh
@@ -3,4 +3,4 @@ MNPBASE=`dirname $0`/..
 MNPLIB=$MNPBASE/lib
 cd $MNPLIB
 MNPTEST=../test
-cat lisp.lisp lib.lisp integer.lisp $MNPTEST/test_mult.lisp | python mnplisp.py --script
+cat lisp.lisp lib.lisp integer.lisp $MNPTEST/test_mult.lisp | python mnplisp.py --script $*
