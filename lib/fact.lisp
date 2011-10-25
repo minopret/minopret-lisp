e934fce3829fa5abd0ba59fc7f67a25b771cfb64
Aaron Mansheim <minopret@gmail.com>
2011-10-25 05:34:46 -0400

    inner lambdas

--- lib/fact.lisp
+++ lib/fact.lisp
@@ -1,18 +1,18 @@
 (label fact-bal3 (lambda (n) (cond
-    ; TODO preclude n < 0
+    ((eq (car n) '-) '(+))  ; just return 1 for negative n
     ((equal n '(0)) '(+))
-    (           t    (fact-bal3-n n '(+))) )))
-
-(label fact-bal3-n (lambda (k fact-bal3-n-k) (cond
-    ((equal k '(0)) fact-bal3-n-k)
-    (           t  (fact-bal3-n (bal3-add k '(-)) (bal3-mult k fact-bal3-n-k))) )))
+    ( t              (
+        (label fact-bal3-n (lambda (k fact-bal3-n-k) (cond
+            ((equal k '(0)) fact-bal3-n-k)
+            ( t            (fact-bal3-n (bal3-add k '(-))
+                                        (bal3-mult k fact-bal3-n-k) )) )))
+         n '(+) )) )))
 
 (label fact-bin (lambda (n) (cond
-    ; TODO preclude n < 0
     ((equal n '(())) '( t))
-    (            t    (fact-bin-n n '( t))) )))
-
-(label fact-bin-n (lambda (k fact-bin-n-k) (cond
-    ((equal k '(())) fact-bin-n-k)
-    (            t  (fact-bin-n (bin-pred k) (bin-mult k fact-bin-n-k))) )))
-
+    ( t               (
+        (label fact-bin-n (lambda (k fact-bin-n-k) (cond
+            ((equal k '(())) fact-bin-n-k)
+            ( t             (fact-bin-n (bin-pred k)
+                                        (bin-mult k fact-bin-n-k) )) )))
+         n '( t) )) )))
--- lib/fib.lisp
+++ lib/fib.lisp
@@ -1,20 +1,21 @@
 (label fib-bal3 (lambda (n) (cond
-    ; TODO preclude n < 0
+    ((eq (car n) '-) '(0))  ; just return zero for negative n
     ((equal n '(0)) '(0))
-    (           t    (fib-bal3-n n '(0) '(+))) )))
+    ( t              (
+        (label fib-bal3-n (lambda (k fibn-k fibn-k+1) (cond
+            ((equal k '(+)) fibn-k+1)
+            ( t            (fib-bal3-n (bal3-add k '(-))
+                                        fibn-k+1
+                                       (bal3-add fibn-k fibn-k+1) )) )))
+         n '(0) '(+) )) )))
+
 
 (label fib-bin (lambda (n) (cond
-    ; TODO preclude n < 0
     ((equal n '(())) '(()))
-    (           t    (fib-bin-n n '(()) '(t))) )))
-
-
-(label fib-bal3-n (lambda (k fibn-k fibn-k+1) (cond
-    ((equal k '(+)) fibn-k+1)
-    (           t  (fib-bal3-n (bal3-add k '(-))
-                                fibn-k+1
-                               (bal3-add fibn-k fibn-k+1) )) )))
-
-(label fib-bin-n (lambda (k fibn-k fibn-k+1) (cond
-    ((equal k '(t)) fibn-k+1)
-    (           t  (fib-bin-n (bin-pred k) fibn-k+1 (bin-add fibn-k fibn-k+1))) )))
+    ( t              (
+        (label fib-bin-n (lambda (k fibn-k fibn-k+1) (cond
+            ((equal k '(t)) fibn-k+1)
+            ( t            (fib-bin-n (bin-pred k)
+                                       fibn-k+1
+                                      (bin-add fibn-k fibn-k+1) )) )))
+         n '(()) '(t) )) )))
