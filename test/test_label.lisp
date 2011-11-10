f9783010eef1168cd2a359ff28f81de9fbfad45f
Aaron Mansheim <minopret@gmail.com>
2011-11-09 23:04:00 -0500

    modified test to return "t" like all the other tests

--- test/test_label.lisp
+++ test/test_label.lisp
@@ -1,6 +1,14 @@
-(   (label rev
-        (lambda (xr x) (cond
+(   (label equal (lambda (x y) (cond
+        ((atom x)   (cond ((atom y) (eq x y)) (t ())))
+        ((atom y)   ())
+        ((equal (car x) (car y))
+                    (equal (cdr x) (cdr y)))
+        ( t         ()) )))
+
+    (   (label rev (lambda (xr x) (cond
             ((eq x ())  xr)
-            ( t        (rev (cons (car x) xr) (cdr x))) )) )
-    ()
-   '(a b c d) )
+            ( t        (rev (cons (car x) xr) (cdr x))) )))
+        ()
+       '(a b c d) )
+
+   '(d c b a) )
