e1a8d563eeee87756362ccb7536068b2c544d8a9
Aaron Mansheim <minopret@gmail.com>
2011-11-14 06:01:53 -0500

    changed library file contents semantics from evaluable expressions into storable environment defs

--- test/test_binary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-MNPBASE=`dirname $0`/..
-MNPLIB=$MNPBASE/lib
-cd $MNPLIB
-MNPTEST=../test
-cat lisp.lisp lib.lisp integer.lisp binary.lisp $MNPTEST/test_binary.lisp | python mnplisp.py --script $*
