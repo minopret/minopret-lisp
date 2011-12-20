654278035328f2740f5b5aef8aa3d0d069154de4
Aaron Mansheim <minopret@gmail.com>
2011-12-19 23:58:06 -0500

    moved complex part of Expr.__str__ to a helper method

--- lib/mnpexpr.py
+++ lib/mnpexpr.py
@@ -5,11 +5,13 @@ Symbol = str
 
 
 class Expr(tuple):
-
-    def __str__(self):
+    def str_helper(self):
         if len(self) == 2 and self[0] == 'quote':
             s = "'"
-            s += str(self[1])
+            if isinstance(self[1], Symbol):
+                s += self[1]
+            else:
+                s += Expr(tuple(self[1])).str_helper()
         else:
             s = '('
             is_first = True
@@ -18,6 +20,13 @@ class Expr(tuple):
                     is_first = False
                 else:
                     s += ' '
-                s += str(x)
+
+                if isinstance(x, Symbol):
+                    s += x
+                else:
+                    s += Expr(tuple(x)).str_helper()
             s += ')'
         return s
+
+    def __str__(self):
+        return self.str_helper()
