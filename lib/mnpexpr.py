6de9d26aea2190fe261a8e9152a55abcd40bcc4a
Aaron Mansheim <minopret@gmail.com>
2011-12-22 08:05:47 -0500

    Added indentation in (not very) pretty-printing

--- lib/mnpexpr.py
+++ lib/mnpexpr.py
@@ -5,13 +5,13 @@ Symbol = str
 
 
 class Expr(tuple):
-    def str_helper(self):
+    def to_string(self):
         if len(self) == 2 and self[0] == 'quote':
             s = "'"
             if isinstance(self[1], Symbol):
                 s += self[1]
             else:
-                s += Expr(tuple(self[1])).str_helper()
+                s += Expr(tuple(self[1])).to_string()
         else:
             s = '('
             is_first = True
@@ -24,9 +24,30 @@ class Expr(tuple):
                 if isinstance(x, Symbol):
                     s += x
                 else:
-                    s += Expr(tuple(x)).str_helper()
+                    s += Expr(tuple(x)).to_string()
             s += ')'
         return s
 
+    def indent(self):
+        s = self.to_string()
+        t = ''
+        i = 0
+        breakpoints = (
+            lambda s: s.rfind(')', 0, 70),
+            lambda s: s.rfind(' ', 0, 70),
+            lambda s: s.find(' '),
+            lambda s: s.find(')'),
+            lambda s: len(s) - 1,
+        )
+        while len(s) > 0:
+            for b in breakpoints:
+                p = b(s) + 1
+                if p > 0:
+                    break
+            i += s[0:p].count('(') - s[0:p].count(')')
+            t += s[0:p] + "\n" + (' ' * i)
+            s = s[p:]
+        return t
+
     def __str__(self):
-        return self.str_helper()
+        return self.indent()
