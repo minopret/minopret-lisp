788b6de436f5fe1da1cea3b6b21dde9ddd1cb5ae
Aaron Mansheim <minopret@gmail.com>
2012-10-12 06:35:32 -0400

    removing confusing and in some ways perfunctory testing of incomplete function expr->str

--- test/inactive_test_expr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-MNPBASE=`dirname $0`/..
-MNPLIB=$MNPBASE/lib
-cd $MNPLIB
-MNPTEST=../test
-cat $MNPTEST/inactive_test_expr.lisp | python mnplisp.py \
-    -M lisp lib integer binary mnpexpr --script $*
